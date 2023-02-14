92b6a92cd3b8b9d07d1a03d433425502f15d3433
Kathleen Tyner <kathleentyner@gmail.com>
2023-02-14 15:07:25 -0500

    Loop exercises completed because I am a rockstar

---
+++ main.js
@@ -0,0 +1,8 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+var i = 0
+for(var i = 0; i < cohortOneStudents.length; i++) {console.log(cohortOneStudents[i])}; 
+
+
+var interests = ["training animals", "knitting", "hiking", "reading", "watching tv"]
+
+for(var i = 0; i < interests.length; i++) {console.log(`One of my interests is: ${interests[i]}`)}
